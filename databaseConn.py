647b671fd312324c18d275727639e3df38630016
Shirozo <caranzoneilbryan@gmail.com>
2023-12-21 13:04:29 +0800

    Table responsiveness Fix

--- databaseConn.py
+++ databaseConn.py
@@ -6,5 +6,5 @@ def createConnection() -> Union[QSqlDatabase, None]:
     db.setHostName("127.0.0.1")
     db.setDatabaseName("OOP_finalProject")
     db.setUserName("root")
-    db.setPassword("essuxlug")
+    db.setPassword("Bry01202004")
     return db
