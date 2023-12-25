e87c2028184235ba05e79236702465544cca6bb3
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-12-25 17:24:41 +0800

    Database Documenatation

--- databaseConn.py
+++ databaseConn.py
@@ -2,9 +2,27 @@ from PyQt5.QtSql import QSqlDatabase
 from typing import Union
 
 def createConnection() -> Union[QSqlDatabase, None]:
-    db = QSqlDatabase.addDatabase("QMYSQL")
-    db.setHostName("127.0.0.1")
-    db.setDatabaseName("OOP_finalProject")
-    db.setUserName("root")
-    db.setPassword("Bry01202004")
-    return db
+    """
+    Create and return a connection to the MySQL database.
+
+    Returns:
+    - QSqlDatabase: A connection to the MySQL database.
+    - None: If the connection cannot be established.
+
+    Usage:
+    ```python
+    connection = createConnection()
+    if connection:
+        # Perform database operations using the 'connection' object
+    else:
+        # Handle connection failure
+    ```
+    This function creates a connection to a MySQL database using the PyQt5.QtSql module. The connection parameters (hostname, database name, username, and password) are set within the function.
+    """
+
+    db = QSqlDatabase.addDatabase("QMYSQL")     #Add database engine
+    db.setHostName("127.0.0.1")                 #Set the host
+    db.setDatabaseName("OOP_finalProject")      #Set the database you want to use
+    db.setUserName("root")                      #Set the username
+    db.setPassword("Bry01202004")               #Set the password
+    return db                                   #Return the database
